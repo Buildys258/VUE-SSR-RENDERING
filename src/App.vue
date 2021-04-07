3f91a08d946cbd6ce1180a13c5aa023ddaa2c6ae
dys515707454 <515707454@qq.com>
2021-04-07 18:54:13 +0800

    服务端打包过程完成一半，期待源码

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+    <div id="app">
+        <div class="demo" @click="onClick">
+            一段内容
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    methods: {
+        onClick() {
+            console.log('click!!!!!!!!!!!!')
+        }
+    }
+}
+</script>
+<style lang="">
+.demo {
+    width: 300px;
+    height: 300px;
+    background-color: orange;
+}
+</style>
---
+++ src/entry-server.js
@@ -0,0 +1,5 @@
+import { createApp } from './app'
+
+export default () => {
+    const {app} = createApp()
+} //每次请求就会执行这个函数
